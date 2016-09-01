54a371053ad776803f05269a07beab8757c32490
dargaCode <darga.code@gmail.com>
2016-08-31 17:18:26 -0700

    Request wikipedia information for each bear - Loop through valid bears and request a snippet of   the first wikipedia entry found. - Requires another callback function to wait for all   of the responses to come back.
    PS
    - Events could be something to look into after this.
    - The tutorial also includes a recursive function to
       make sure the requests complete in a prescribed
       order

--- app.js
+++ app.js
@@ -3,10 +3,12 @@
 
 // fs is a part of the node api (ie not downloaded)
 const fs = require('fs');
+const request = require('request');
 
 // CONSTANTS
 
 const DICT_PATH = './dict/bears.dictionary';
+const WIKI_INFO_LENGTH = 200;
 
 // FUNCTIONS
 
@@ -32,6 +34,27 @@ function getBears(bearsPath, callback) {
   }
 }
 
+function getBearDescription(bears, callback) {
+  let count = bears.length;
+  let resultObj = {};
+
+  bears.forEach(function(element) {
+    getWikipediaInfo(element, function(err, intro) {
+
+      resultObj[element.toUpperCase()] = intro;
+      nextBear();
+    });
+  })
+
+  function nextBear() {
+    count--;
+    console.log("Requests remaining:", count);
+    if (count < 1) {
+      callback(resultObj);
+    }
+  }
+}
+
 // HELPERS
 
 function getLineArrayFromFileBuffer(fileBuffer) {
@@ -44,8 +67,38 @@ function filterArrayByArray(arrayA, arrayB) {
   });
 }
 
+/* the tutorial loaded images instead, but I guess he was using a feature of io.js that didn't survive the merge
+back into node.js? */
+function getWikipediaInfo(searchTerm, callback) {
+  const searchUrl = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=${searchTerm}`;
+
+  request(searchUrl, function(error, response, body) {
+    if (error || response.statusCode != 200) {
+      callback([response.statusCode, error]);
+    }
+
+    const searchResults = JSON.parse(body);
+    const trimmedInfo = getTrimmedInfo(searchResults);
+
+    callback(null, trimmedInfo);
+  });
+}
+
+function getTrimmedInfo(searchResults) {
+  const pages = searchResults.query.pages;
+  const firstPageKey = Object.keys(pages)[0];
+  const summary = pages[firstPageKey].extract;
+  const trimmedInfo = summary.slice(0, WIKI_INFO_LENGTH);
+
+  return trimmedInfo;
+}
+
 // MAIN
 
 getBears('bears-input.txt', function(err, bears) {
   console.log('VALID BEAR LIST:', bears, '\n');
+
+  getBearDescription(bears, function(result) {
+    console.log('\nTHE FINAL RESULT IS: \n\n', result);
+  });
 });
